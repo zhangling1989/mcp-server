47c390a2596ee5a899fd9939e95763c0b44a695f
ifamilystory <ifamilystory@aliyun.com>
2025-06-02 13:19:20 +0800

    # mcp

---
+++ mvp_server.py
@@ -0,0 +1,36 @@
+from typing import Any
+import  httpx
+
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP(
+    name="mcp-server",
+    port=5050,
+    stateless_http=False,
+    json_response=True,
+    streamable_http_path="/mcp"
+)
+
+
+
+@mcp.tool()
+async  def see(xing: str) -> str:
+    """获取用户名
+
+    Args:
+        xing: 用户的姓
+    """
+    return "张领"
+
+@mcp.tool()
+async  def stdio(name: str) -> str:
+    """获取用户的性别
+
+    Args:
+        xing: 用户的姓名
+    """
+    return "男"
+
+
+if __name__ == '__main__':
+    mcp.run(transport="streamable-http")  # uv --dir E:\dev\ai\mcp_demo  run mcp_server.py
